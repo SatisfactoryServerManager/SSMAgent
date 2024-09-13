f2e7d537a264dd78b297113bd5860512fd74561e
mrhid6 <dominic.richardson1@hotmail.co.uk>
2024-09-13 10:15:36 +0100

    fix: Fixed docker cleanup script

--- entry.sh
+++ entry.sh
@@ -1,3 +1,30 @@
 #!/bin/bash
 
-su ssm -c "/opt/SSMAgent/SSMAgent -name=$SSM_NAME -apikey=$SSM_APIKEY -url=$SSM_URL"
+EXE="/opt/SSMAgent/SSMAgent"
+
+#Define cleanup procedure
+cleanup() {
+    echo "Container stopped, performing cleanup..."
+    pid=$(ps -ef | awk '$8=="${EXE}" {print $2}')
+    kill -INT $pid
+
+    while true; do
+        echo "Waiting for process to finish"
+        pid=$(ps -ef | awk '$8=="${EXE}" {print $2}')
+        if [ "$pid" == "" ]; then
+            break
+        fi
+        sleep 5
+    done
+    exit 0
+}
+
+#Trap SIGTERM
+trap 'cleanup' SIGTERM
+
+hostname
+
+su ssm -c "${EXE} -name=$SSM_NAME -apikey=$SSM_APIKEY -url=$SSM_URL" &
+
+wait $!
+sleep 40
