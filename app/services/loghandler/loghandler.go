7a7cc6b3256ea0d778770e82a888ab730603e80c
mrhid6 <dominic.richardson1@hotmail.co.uk>
2025-11-08 16:24:08 +0000

    feat: new log handling

--- app/services/loghandler/loghandler.go
+++ app/services/loghandler/loghandler.go
@@ -1,36 +1,71 @@
 package loghandler
 
 import (
-	"os"
 	"path/filepath"
-	"time"
 
 	"github.com/SatisfactoryServerManager/SSMAgent/app/api"
 	"github.com/SatisfactoryServerManager/SSMAgent/app/config"
 	"github.com/SatisfactoryServerManager/SSMAgent/app/utils"
+	"github.com/hpcloud/tail"
 )
 
 var (
 	_quit = make(chan int)
-
-	FactoryGameLogTime time.Time
+	tails []*tail.Tail
 )
+
+func watchFile(filePath string) (*tail.Tail, error) {
+	t, err := tail.TailFile(filePath, tail.Config{
+		Follow:    true,
+		ReOpen:    true,
+		MustExist: false,
+		Poll:      true,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	go func() {
+		for line := range t.Lines {
+			if line.Err != nil {
+				utils.ErrorLogger.Printf("Error reading line from %s: %v\n", filePath, line.Err)
+				continue
+			}
+			// Send the log update to the API
+			if err := api.SendFile("/api/v1/agent/upload/log", filePath); err != nil {
+				utils.ErrorLogger.Printf("Error sending log file %s: %v\n", filePath, err)
+			}
+		}
+	}()
+
+	return t, nil
+}
 
 func InitLogHandler() {
 	utils.InfoLogger.Println("Initialising Log Handler...")
 
-	ticker := time.NewTicker(30 * time.Second)
-	go func() {
-		for {
-			select {
-			case <-ticker.C:
-				SendLogFiles()
-			case <-_quit:
-				ticker.Stop()
-				return
-			}
+	// Watch SSM Agent log
+	ssmlogfile := filepath.Join(config.GetConfig().LogDir, "SSMAgent-combined.log")
+	if utils.CheckFileExists(ssmlogfile) {
+		t, err := watchFile(ssmlogfile)
+		if err != nil {
+			utils.ErrorLogger.Printf("Error setting up tail for SSM log: %v\n", err)
+		} else {
+			tails = append(tails, t)
 		}
-	}()
+	}
+
+	// Watch FactoryGame log
+	gamelogdir := filepath.Join(config.GetConfig().SFDir, "FactoryGame", "Saved", "Logs")
+	utils.CreateFolder(gamelogdir)
+	gamelogfile := filepath.Join(gamelogdir, "FactoryGame.log")
+
+	t, err := watchFile(gamelogfile)
+	if err != nil {
+		utils.ErrorLogger.Printf("Error setting up tail for game log: %v\n", err)
+	} else {
+		tails = append(tails, t)
+	}
 
 	utils.InfoLogger.Println("Initialised Log Handler")
 }
@@ -38,50 +73,15 @@ func InitLogHandler() {
 func ShutdownLogHandler() error {
 	utils.InfoLogger.Println("Shutting down Log Handler")
 
+	// Stop all tails
+	for _, t := range tails {
+		t.Stop()
+		t.Cleanup()
+	}
+	tails = nil
+
 	_quit <- 0
 
 	utils.InfoLogger.Println("Shutdown Log Handler")
 	return nil
-}
-
-func SendLogFiles() {
-	utils.DebugLogger.Println("Sending Log Files")
-
-	ssmlogfile := filepath.Join(config.GetConfig().LogDir, "SSMAgent-combined.log")
-
-	if utils.CheckFileExists(ssmlogfile) {
-		if err := api.SendFile("/api/v1/agent/upload/log", ssmlogfile); err != nil {
-			utils.ErrorLogger.Println(err.Error())
-		}
-	}
-
-	gamelogdir := filepath.Join(
-		config.GetConfig().SFDir,
-		"FactoryGame",
-		"Saved",
-		"Logs",
-	)
-
-	utils.CreateFolder(gamelogdir)
-
-	gamelogfile := filepath.Join(
-		gamelogdir,
-		"FactoryGame.log",
-	)
-
-	if utils.CheckFileExists(gamelogfile) {
-
-		stats, err := os.Stat(gamelogfile)
-
-		if err != nil {
-			return
-		}
-
-		if stats.ModTime().After(FactoryGameLogTime) {
-			if err := api.SendFile("/api/v1/agent/upload/log", gamelogfile); err != nil {
-				utils.ErrorLogger.Println(err.Error())
-			}
-			FactoryGameLogTime = stats.ModTime()
-		}
-	}
 }
--- go.mod
+++ go.mod
@@ -11,9 +11,12 @@ require (
 require (
 	github.com/SatisfactoryServerManager/ssmcloud-resources v0.0.16 // indirect
 	github.com/go-ole/go-ole v1.3.0 // indirect
+	github.com/hpcloud/tail v1.0.0 // indirect
 	github.com/stretchr/testify v1.10.0 // indirect
 	github.com/tklauser/go-sysconf v0.3.15 // indirect
 	github.com/tklauser/numcpus v0.10.0 // indirect
 	github.com/yusufpapurcu/wmi v1.2.4 // indirect
 	golang.org/x/sys v0.37.0 // indirect
+	gopkg.in/fsnotify.v1 v1.4.7 // indirect
+	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
 )
